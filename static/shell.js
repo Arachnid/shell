0a75f5f0ae7ed5eef790f2fd85cdf329216d9871
Nick Johnson <nickjohnson@google.com>
2012-04-23 15:50:42 +1000

    Fixed history in Chrome

--- static/shell.js
+++ static/shell.js
@@ -94,12 +94,12 @@ shell.onPromptKeyPress = function(event) {
   }
 
   // should we pull something from the history?
-  if (event.ctrlKey && event.keyCode == 38 /* up arrow */) {
+  if ((event.ctrlKey && event.keyCode == 38) || event.keyCode == 63232 /* up arrow */) {
     if (this.historyCursor > 0) {
       statement.value = this.history[--this.historyCursor];
     }
     return false;
-  } else if (event.ctrlKey && event.keyCode == 40 /* down arrow */) {
+  } else if ((event.ctrlKey && event.keyCode == 40) || event.keyCode == 63233 /* down arrow */) {
     if (this.historyCursor < this.history.length - 1) {
       statement.value = this.history[++this.historyCursor];
     }
